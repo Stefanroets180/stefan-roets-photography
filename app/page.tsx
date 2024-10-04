a9788e8952de94437f6b3db7df32f9b3893b5381
Stefan Roets <stefanroetscode@gmail.com>
2024-10-04 12:56:31 +0200

    font busy with

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@
 import Link from "next/link";
 import {Tab} from "@headlessui/react";
 import Masonry from 'react-masonry-css'
+import Image from 'next/image'
+import classNames from 'classnames'
 
 
 const tabs = [
@@ -27,8 +29,10 @@ export default function Home() {
             <p className="bg-gradient-to-r from-[black] to-[#1c1c1c] -[0px] flex justify-center items-center ">Photography Portfolio</p>
 */}
 
-            <header className="fixed top-0 w-full z-10 flex justify-between items-center h-[90px] px-6">
-                <div></div>
+            <header className="fixed top-0 w-full z-10 flex justify-between items-center h-[90px] px-12">
+                <span className="uppercase text-lg font-medium">
+                    Stefan's Photography Portfolio
+                </span>
                 <Link href="#"
                       className="rotate-3xl bg-white px-4 py-2 text-black font-medium rounded-md hover:bg-gray-500 transition duration-300"
                 >
@@ -39,13 +43,15 @@ export default function Home() {
             <main className="pt-[110px]">
                 <div className="flex flex-col items-center h-full">
                     <Tab.Group >
-                        <Tab.List className="flex items-center gap-20">
-
+                        <Tab.List className="flex items-center gap-20 uppercase">
                             {tabs.map(tab => (
                                 <Tab key={tab.key} className="p-2" >
                                     {({selected}) => (
                                         <span
-                                            className={selected ? "text-white" : "text-stone-600"}
+                                            className={classNames(
+                                                "uppercase text-lg",
+                                                selected ? "text-white" : "text-stone-600"
+                                            )}
                                         >
                                              {tab.display}
                                         </span>
@@ -55,11 +61,15 @@ export default function Home() {
                         </Tab.List>
                         <Tab.Panels className="h-full max-w-[900px] w-full p-2 sm:p-4 my-6">
                             <Tab.Panel className="overflow-auto">
-                                <Masonry breakpointCols={2} className="flex gap-2" columnClassName="">
-                                    <img src="/pwl-1.webp" alt="pwl-1" className="my-2"/>
-                                    <img src="/pwl-2.webp" alt="pwl-2" className="my-2"/>
-                                    <img src="/pwl-3.webp" alt="pwl-3" className="my-2"/>
-                                    <img src="/pwl-4.webp" alt="pwl-4" className="my-2"/>
+                                <Masonry
+                                    breakpointCols={2}
+                                    className="flex gap-4"
+                                    columnClassName=""
+                                >
+                                    <img src="/pwl-1.webp" alt="pwl-1" className="my-4"/>
+                                    <img src="/pwl-2.webp" alt="pwl-2" className="my-4"/>
+                                    <img src="/pwl-3.webp" alt="pwl-3" className="my-4"/>
+                                    <img src="/pwl-4.webp" alt="pwl-4" className="my-4"/>
                                 </Masonry>
                             </Tab.Panel>
                             <Tab.Panel>Painting with Light</Tab.Panel>
@@ -70,8 +80,8 @@ export default function Home() {
 
             </main>
 
-            <footer className="bg-[black] -[10px] flex justify-center items-center ">
-                <p> Stefan Photography Portfolio </p>
+            <footer className="bg-[black] -[10px] flex justify-center items-center uppercase text-lg font-medium">
+                <p> Stefan's Photography Portfolio </p>
             </footer>
         </div>
     );
